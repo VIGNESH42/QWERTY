b9df4bb2f6b9f6745ed65f6bdbf026898ee3dc78
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2017-09-05 17:00:01 +0530

    Create 28.c

---
+++ 28.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int i;char c;
+printf("print a character");
+scanf("%c",&c);
+if(c=='q')
+{
+int a,b,c;
+a=10;
+b=20;
+c=a+b;
+printf("%d",c);
+}
+else
+{
+printf("enter a valid input");
+}}
