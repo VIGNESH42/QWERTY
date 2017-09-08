ff455ca38f528813fd9e504f5293c0177acf6eaf
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2017-09-08 16:27:56 +0530

    Create 36.c

---
+++ 36.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main() {
+   int a, b, temp;
+   printf("Enter the value of a and b: ");
+   scanf("%d %d", &a, &b);
+   printf("Before swapping a=%d, b=%d ", a, b);
+   temp = a;
+   a = b;
+   b = temp;
+   printf("After swapping a=%d, b=%d", a, b);
+ 
+}
