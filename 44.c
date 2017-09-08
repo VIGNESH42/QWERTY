2d791304c0d6bf61d2bb86b2a2113ed742922d9d
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2017-09-08 16:51:52 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter anumber range between 1 to 9");
+scanf("%d",&n);
+if((n<9)&&(n>1))
+{
+printf("your number is in range");
+}
+else
+{
+printf("your number is not in range");
+}
+}
