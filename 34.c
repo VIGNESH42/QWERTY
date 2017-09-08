b83cc9eb4b9ae71e6939f3fdcacae8b6e4ba171b
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2017-09-08 16:16:28 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+char line[81], ctr;
+end = 0,
+characters = 0,
+printf(“KEY IN THE TEXT.\n”);
+printf(“GIVE ONE SPACE AFTER EACH WORD.\n”);
+printf(“WHEN COMPLETED, PRESS ‘RETURN’.\n\n”);
+if(line[0] == ”)
+break ;
+characters = characters + strlen(line);
+
+
+printf(“Number of characters = %d\n”, characters);
+}
