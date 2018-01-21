23255c24558eee667fd94174f1031be970b04950
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-01-21 16:55:26 +0530

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+  int num;
+  printf("\n Enter a number:");
+  scanf("%d",&num);
+  if(num%7==0)
+  {
+  printf("\n %d is divisible by 7");
+  }
+ else
+{
+printf("not divisible by 7");
+}
+}
