2d95040dd867059dd75d5b942a6a24598625ccd4
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-01-31 16:45:06 +0530

    Create 80.c

---
+++ 80.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int a[10];
+printf("enter the value");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<10;i++)
+{
+if((a[i]%2)!=0)
+{
+printf("%d",&a[i]);
+}
+}
