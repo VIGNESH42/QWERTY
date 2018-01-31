a449691840b96175eceb32a37d4806560813eab4
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-01-31 16:28:32 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the limit");
+scanf("%d %d",&a &b);
+printf("enter the number to be checked");
+scanf("%d",&c);
+if((c<<a)||(c<b))
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
