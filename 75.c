faec19b5c45540d5d465ded6f66c806e6a67edf3
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-01-31 16:57:18 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+int i;
+printf("enter the values");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+a[5]=*;
+for(i=0;i<10;i++)
+{
+printf("%d\n",a[i]);
+}
+}
