e5243ebee2349009ae145b1d472ed88588f16418
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-01-31 15:51:48 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+char a[10];
+int i,count=0;
+printf("enter the sentence");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<10;i++)
+{
+if(a[i]==" ")
+{
+count=count+1;
+}
+}
+printf("the no of words is %d",count);
+}
