90096eacd9548cc7bc759c8ce520599740ac1786
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-02-07 16:18:14 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+char str={"kavi10"};
+
+int i,flag ;
+for(i=o;i=!'\0';i++)
+{
+if(((str[i]>='a')||(str[i]<='z'))&&((str[i]>=0)||(str[i]<=9)))
+{
+flag=0;
+}
+else
+{
+flag=1;
+}}
+if(flag==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+}
