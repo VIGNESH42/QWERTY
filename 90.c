4025c74a934fe278ddebaf9de022b607c08ec65d
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-02-09 15:05:01 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+char str[10];
+int i;
+printf("enter the alphanumeric string");
+gets(str);
+for(i=0;str[i]>=5;i++)
+{
+if(str[i]>'0' && str[i]<'9')
+{
+printf("%c",str[i]);
+}
+}
+}
