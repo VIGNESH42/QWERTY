91c5d6fd67db02c989ee1c314582744fa9eb8b20
kavinandhu1001 <30829042+kavinandhu1001@users.noreply.github.com>
2018-02-09 13:59:29 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int i,a,b,c,d;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("enter the number ");
+scanf("%d",&a);
+printf("enter the number ");
+scanf("%d",&b);
+c=a-b;
+printf("difference is %d",&c);
+}
+}
